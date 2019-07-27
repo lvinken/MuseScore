36b000365a855413b8fced71f35908e320ac4a46
Leon Vinken <leon.vinken@gmail.com>
2019-07-27 20:51:38 +0200

    handle initial tempo

--- mtest/mnx/io/tst_mnx_io.cpp
+++ mtest/mnx/io/tst_mnx_io.cpp
@@ -63,6 +63,7 @@ private slots:
       void multiPart() { mnxIoTest("testMultiPart"); }
       void multiStaff() { mnxIoTest("testMultiStaff"); }
       void notesRests1() { mnxIoTest("testNotesRests1"); }
+      void tempo0() { mnxIoTest("testTempo0"); }
       void tuplets0() { mnxIoTest("testTuplets0"); }
       void voicePiano1() { mnxIoTest("testVoicePiano1"); }
       void voicePiano2() { mnxIoTest("testVoicePiano2"); }
