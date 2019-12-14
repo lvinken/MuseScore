3514155109031e529a6aa06036b5aa48354b5ec1
Leon Vinken <leon.vinken@gmail.com>
2019-12-14 11:03:54 +0100

    add tie testfile

--- mtest/mnx/io/tst_mnx_io.cpp
+++ mtest/mnx/io/tst_mnx_io.cpp
@@ -70,6 +70,7 @@ private slots:
       void notesRests1() { mnxIoTest("testNotesRests1"); }
       void octaveShift1() { mnxIoTest("testOctaveShift1"); }
       void tempo0() { mnxIoTest("testTempo0"); }
+      void ties1() { mnxIoTest("testTies1"); }
       void tuplets0() { mnxIoTest("testTuplets0"); }
       void voicePiano1() { mnxIoTest("testVoicePiano1"); }
       void voicePiano2() { mnxIoTest("testVoicePiano2"); }
