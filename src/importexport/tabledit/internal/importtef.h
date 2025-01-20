452f777fe0f52a58360c500912be5abfbfa04a92
Leon Vinken <leon.vinken@gmail.com>
2025-01-20 21:15:01 +0100

    update readTefHeader(), cleanup

--- src/importexport/tabledit/internal/importtef.h
+++ src/importexport/tabledit/internal/importtef.h
@@ -32,11 +32,12 @@ namespace mu::iex::tabledit {
 class TablEdit
 {
     muse::io::IODevice* _file = nullptr;
-    mu::engraving::MasterScore* score = nullptr;
+    /* todo mu::engraving::MasterScore* score = nullptr; */
 
     uint8_t readUInt8();
     uint16_t readUInt16();
     uint32_t readUInt32();
+    string readText();
 
     struct TefHeader {
         int version { 0 };
@@ -45,16 +46,18 @@ class TablEdit
         int chorus { 0 };
         int reverb { 0 };
         int syncope { 0 };
-        unsigned int securityCode { 0 };
+        unsigned int securityCode { 0 }; // open file only if 0
         unsigned int securityFlags { 0 };
-        int tbed { 0 };
-        int wOldNum { 0 };
-        int wFormat { 0 };
+        int tbed { 0 }; // should be 0x64656274 ("tbed"), but is sometimes reversed (?)
+        int wOldNum { 0 }; // must be 4
+        int wFormat { 0 }; // hibyte > 9: v3.00 file, hibyte > 10: may no longer match v3.00 spec
         std::string title;
         std::string subTitle;
         std::string comment;
         std::string internetLink;
         std::string copyright;
+        int i202 { 0 };
+        int i204 { 0 };
     };
 
     void readTefHeader();
@@ -62,8 +65,8 @@ class TablEdit
     TefHeader tefHeader;
 
 public:
-    TablEdit(muse::io::IODevice* f, mu::engraving::MasterScore* s)
-        : _file(f), score(s) {}
+    TablEdit(muse::io::IODevice* f, mu::engraving::MasterScore* /* todo s */)
+        : _file(f) /* todo , score(s) */ {}
     mu::engraving::Err import();
 };
 
