c4a598610156c899fc77f3c660fb3a0953319283
Leon Vinken <leon.vinken@gmail.com>
2025-07-26 09:20:07 +0200

    complete autotest

--- src/importexport/tabledit/tests/tabledit_tests.cpp
+++ src/importexport/tabledit/tests/tabledit_tests.cpp
@@ -22,20 +22,14 @@
 
 #include <gtest/gtest.h>
 
-#include "engraving/engravingerrors.h"
-#include "engraving/dom/masterscore.h"
-
 #include "engraving/tests/utils/scorecomp.h"
 #include "engraving/tests/utils/scorerw.h"
 
-#include "project/inotationreader.h"
-
 #include "importexport/tabledit/internal/tableditreader.h"
 
-using namespace mu;
 using namespace mu::engraving;
 
-static const String TABLEDIT_DIR("data/");
+static const muse::String TABLEDIT_DIR("data/");
 
 class TablEdit_Tests : public ::testing::Test
 {
@@ -50,7 +44,7 @@ public:
 
 void TablEdit_Tests::tefReadTest(const char* file)
 {
-    auto importFunc = [](MasterScore* score, const muse::io::path_t& path) -> engraving::Err {
+    auto importFunc = [](MasterScore* score, const muse::io::path_t& path) -> Err {
         mu::iex::tabledit::TablEditReader tablEditReader;
         return tablEditReader.import(score, path);
     };
@@ -64,6 +58,114 @@ void TablEdit_Tests::tefReadTest(const char* file)
     delete score;
 }
 
-TEST_F(TablEdit_Tests, tefTest1) {
+TEST_F(TablEdit_Tests, tef_bass) {
+    tefReadTest("bass");
+}
+
+TEST_F(TablEdit_Tests, tef_chord_C_D) {
+    tefReadTest("chord_C_D");
+}
+
+TEST_F(TablEdit_Tests, tef_grace_1) {
+    tefReadTest("grace_1");
+}
+
+TEST_F(TablEdit_Tests, tef_guitar) {
+    tefReadTest("guitar");
+}
+
+TEST_F(TablEdit_Tests, tef_guitar_bass) {
+    tefReadTest("guitar_bass");
+}
+
+TEST_F(TablEdit_Tests, tef_guitar_drop_D) {
+    tefReadTest("guitar_drop_D");
+}
+
+TEST_F(TablEdit_Tests, tef_guitar_new_standard_tuning) {
+    tefReadTest("guitar_new_standard_tuning");
+}
+
+TEST_F(TablEdit_Tests, tef_key_signatures) {
+    tefReadTest("key_signatures");
+}
+
+TEST_F(TablEdit_Tests, tef_key_signatures_2) {
+    tefReadTest("key_signatures_2");
+}
+
+TEST_F(TablEdit_Tests, tef_metadata) {
     tefReadTest("metadata");
+}
+
+TEST_F(TablEdit_Tests, tef_multi_track_rests) {
+    tefReadTest("multi_track_rests");
+}
+
+TEST_F(TablEdit_Tests, tef_notes_dotted) {
+    tefReadTest("notes_dotted");
+}
+
+TEST_F(TablEdit_Tests, tef_notes_normal) {
+    tefReadTest("notes_normal");
+}
+
+TEST_F(TablEdit_Tests, tef_positions) {
+    tefReadTest("positions");
+}
+
+TEST_F(TablEdit_Tests, tef_reading_list_1) {
+    tefReadTest("reading_list_1");
+}
+
+TEST_F(TablEdit_Tests, tef_rests_dotted) {
+    tefReadTest("rests_dotted");
+}
+
+TEST_F(TablEdit_Tests, tef_rests_normal) {
+    tefReadTest("rests_normal");
+}
+
+TEST_F(TablEdit_Tests, tef_staff_text_1) {
+    tefReadTest("staff_text_1");
+}
+
+TEST_F(TablEdit_Tests, tef_staff_text_2) {
+    tefReadTest("staff_text_2");
+}
+
+TEST_F(TablEdit_Tests, tef_tie_1) {
+    tefReadTest("tie_1");
+}
+
+TEST_F(TablEdit_Tests, tef_tie_2) {
+    tefReadTest("tie_2");
+}
+
+TEST_F(TablEdit_Tests, tef_time_signatures) {
+    tefReadTest("time_signatures");
+}
+
+TEST_F(TablEdit_Tests, tef_time_signatures_2) {
+    tefReadTest("time_signatures_2");
+}
+
+TEST_F(TablEdit_Tests, tef_triplet_eighths) {
+    tefReadTest("triplet_eighths");
+}
+
+TEST_F(TablEdit_Tests, tef_triplet_quarters) {
+    tefReadTest("triplet_quarters");
+}
+
+TEST_F(TablEdit_Tests, tef_triplets_mixed) {
+    tefReadTest("triplets_mixed");
+}
+
+TEST_F(TablEdit_Tests, tef_voices) {
+    tefReadTest("voices");
+}
+
+TEST_F(TablEdit_Tests, tef_voices_multi_part) {
+    tefReadTest("voices_multi_part");
 }
